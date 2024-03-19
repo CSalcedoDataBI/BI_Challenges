a75406de222396f811727c8b4e7db6f24e87c00c
Cristobal Salcedo <csalcedo90@gmail.com>
2024-03-19 09:23:59 -0500

    415_EXCEL_CHALLENGE Pyspark

---
+++ 415_EXCEL_CHALLENGE/415_EXCEL_CHALLENGE.py
@@ -0,0 +1,21 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf
+from pyspark.sql.types import BooleanType, LongType
+
+
+spark = SparkSession.builder.appName("CyclopsTriangularNumbers").getOrCreate()
+
+
+def is_cyclops(n):
+    s = str(n)
+    length = len(s)
+    if length % 2 == 0 or length < 3:
+        return False
+    middle_index = length // 2
+    return s[middle_index] == '0' and '0' not in s[:middle_index] + s[middle_index+1:]
+
+is_cyclops_udf = udf(is_cyclops, BooleanType())
+num_range = spark.range(1, 10000) 
+triangular_numbers = num_range.withColumn("Expected Number", (col("id") * (col("id") + 1) / 2).cast(LongType()))
+cyclops_triangular_numbers = triangular_numbers.filter(is_cyclops_udf("Expected Number")).drop("id")
+cyclops_triangular_numbers.show(100)
